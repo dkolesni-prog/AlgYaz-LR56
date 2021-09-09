632857d8307b34a765454f4440754198bb6d8685
dkolesni-prog <63922878+dkolesni-prog@users.noreply.github.com>
2021-09-09 14:04:02 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,8 +4,42 @@
 
 #include <iostream>
 #include "my_vector.h"
-#include<string>
+
 #include <algorithm>
+
+struct Cat{
+public:
+    char* breed;
+    Cat(): breed(nullptr){};
+    Cat(char* breed_): breed( breed_){}
+    Cat& operator=(Cat other){
+        this->breed = other.breed;
+        return *this;
+    }
+    Cat Cat_copy(Cat other_cat){
+        Cat ret(other_cat);
+        return ret;
+    }
+    //присваивание оператор констр копирования
+
+//    friend bool operator<(Cat& first, Cat& second);
+//    friend bool operator>(Cat& first, Cat& second);
+
+};
+
+//bool operator<(Cat& first, Cat& second){
+//    return  first.breed < second.breed;
+//    // if (breed < other.breed) return true;
+//}
+//bool operator>(Cat& first, Cat& second){
+//    return  first.breed > second.breed;
+//    // if (breed < other.breed) return true;
+//}
+//bool operator==(Cat& first, Cat& second){
+//    return  first.breed == second.breed;
+//    // if (breed < other.breed) return true;
+//}
+
 
 
 //1) at +
@@ -34,41 +68,130 @@ int main() {
 
 
 
+//
+//    my_vector<int> test_insert;
+//
+//    test_insert.push_back(5);
+//    test_insert.push_back(9);
+//    test_insert.push_back(3);
+//    test_insert.push_back(4);
+//    test_insert.push_back(5);
+//    test_insert.push_back(6);
+//    test_insert.push_back(7);
+//    test_insert.push_back(8);
+//    test_insert.push_back(9);
 
-    my_vector<int> test_insert;
+//    std::cout << "Before: " << std::endl;
+//    for (const auto& item : test_insert) {
+//        std::cout << item << std::endl;
+//    }
+//    std::cout << "number of elements before: " << test_insert.get_n() << std::endl
+//    << "capacity before: " << test_insert.get_capacity() << std::endl <<
+//    "ext capacity before: " << test_insert.get_ext() << std::endl;
+//
+//    std::cout << "-------------------------" << std::endl;
 
-    test_insert.push_back(1);
-    test_insert.push_back(2);
-    test_insert.push_back(3);
-    test_insert.push_back(4);
-    test_insert.push_back(5);
-
-    std::cout << "Before: " << std::endl;
-    for (const auto& item : test_insert) {
-        std::cout << item << std::endl;
-    }
-
-    std::cout << "number of elements after: " << test_insert.number_of_elements << std::endl
-              << " capacity before: " << test_insert.capacity_ << std::endl;
-
-    std::cout << "-------------------------" << std::endl;
+  //  std::cout <<test_insert[666] << std::endl;
 
 
 
 
     // auto it = test_insert.erase(test_insert.begin() + 2);//  erase по итератору
-     //auto it = test_insert.erase(test_insert.begin() + 2, test_insert.end());// erase по диапазону
+    //auto it = test_insert.erase(test_insert.begin() + 2, test_insert.end());// erase по диапазону
 
-    test_insert.insert(test_insert.begin() + 2, 0);// insert по итератору
-    std::cout << "After: " << std::endl;
-    for (const auto& item : test_insert) {
-        std::cout << item << std::endl;
+//   test_insert.insert(test_insert.begin() + 2, 0);// insert по итератору
+//   test_insert.insert(test_insert.begin() + 2, 0);// insert по итератору
+
+//  test_insert.push_back(6);
+//  test_insert.push_back(7);
+//  test_insert.push_back(8);
+//  test_insert.push_back(9);
+//    std::cout << "After insert: " << std::endl;
+//    for (const auto& item : test_insert) {
+//        std::cout << item << std::endl;
+//    }
+//    std::cout << "number of elements after: " << test_insert.get_n() << std::endl
+//    << "capacity after: " << test_insert.get_capacity() << std::endl << "ext capacity after : "<<
+//     test_insert.get_ext() <<  std::endl;
+//
+//    std::cout << " last element is : " << test_insert.back() << std::endl;
+
+//
+//    std::cout << "number of elements after: " << test_insert.get_n() << std::endl
+//    << " capacity after: " << test_insert.get_capacity() << std::endl;
+//    // std::cout << "deleted: " << " ' " << *it << " ' " << std::endl;
+//
+//    std::cout << "-------------------------" << std::endl;
+//    std::cout << "STL demonstration" << std::endl;
+//    std::sort(test_insert.begin(), test_insert.end());
+//    std::cout << "After sort: " << std::endl;
+//    for (const auto& item : test_insert) {
+//        std::cout << item << std::endl;
+//    }
+//    std::cout << "-------------------------" << std::endl;
+//    std::reverse(test_insert.begin(), test_insert.end());
+//    std::cout << "After reverse: " << std::endl;
+//    for (const auto& item : test_insert) {
+//        std::cout << item << std::endl;
+//    }
+
+    //Cat cats[1];
+    std::cout << "-------------------------" << std::endl;
+    std::cout << "STL with struct demonstration" << std::endl;
+    my_vector<Cat> cattery(5,"To_delete");
+
+    cattery.push_back(Cat{"Siberian"});
+//    cattery.push_back(Cat{"Bengal"});
+//    cattery.push_back(Cat{"Savannah"});
+//    cattery.push_back(Cat("Bengaal"));
+//    cattery.push_back(Cat{"A"});
+//    Cat last = cattery.back();
+//    std::cout << last.breed;
+    //std::sort(cattery.begin(), cattery.end());
+
+    std::sort(
+            cattery.front(),
+            cattery.back(),
+            [] (Cat const & l, Cat const & r) {return l.breed < r.breed;}
+            );
+    std::cout << "After sort: " << std::endl;
+    for (const auto& item : cattery) {
+        std::cout << item.breed << std::endl;
     }
 
-    std::cout << "number of elements after: " << test_insert.number_of_elements << std::endl
-    << " capacity after: " << test_insert.capacity_ << std::endl;
-   // std::cout << "deleted: " << " ' " << *it << " ' " << std::endl;
+
+
+//    std::cout << "constr with params demo: " << std::endl;
+//    my_vector<int> par_vec = my_vector(3, 6);
+//
+//    std::cout << "Number of elements: " << par_vec.get_n() << std::endl;
+//    std::cout << "Capacity is: " << par_vec.get_capacity() << std::endl;
+//    std::cout << "Real capacity is: "<< par_vec.get_ext() << std::endl;
+//
+//    for (auto& item: par_vec) {
+//        std::cout << item << std::endl;
+//    }
+
+
 
 
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- my_vector.h
+++ my_vector.h
@@ -1,187 +1,134 @@
 //
-// Created by Daniil Kolesnik on 18/05/2021.
-//all rights and lefts reserved
+// Created by Daniil Kolesnik on 31/08/2021.
+//
 
-#ifndef INC_5THLABONLY_MY_VECTOR_H
-#define INC_5THLABONLY_MY_VECTOR_H
-
-/*ЗДЕСТ ДОЛЖНЫ БЫТЬ
- * !!!!
-1) at
-2) []
-3) front
-4) back
-5) data
-6) begin
-7) end
-8) empty
-9) size
-10) reserve
-11) capacity
-12) clear
-13) insert (вставка элемента)
-14) erase (удаление по итератору, удаление
-диапазона по итераторам)
-15) push_back
-16) pop_back
-17) resize
-18)swap ----- !!!!!!!
- ПЛЮС
- 1) == +
-2) != +
-3) -> +
-4) * +
-5) ++ (правосторонний) +
-6) ++ (левосторонний) +
-7) -- (правосторонний) + 8) -- (левосторонний) +
-9) [] +
-10) + (сдвиг + итератор)  +
-11) + (итератор + сдвиг)
-12) - (итератор – сдвиг) +
-13) - (итератор – итератор) +
-14) += +
-15) -= +
-16) > +
-17) < +
-18) >= +
-19) <= +
- */
+#ifndef LR5_ISSUERESOLUTION_MY_VECTOR_H
+#define LR5_ISSUERESOLUTION_MY_VECTOR_H
 
 
-template<typename T>
-class my_iterator {
-
-public:
-    using pointer = T*;
-    using reference = T&;
-    using value_type = T;
-    using difference_type = int;
-    using iterator_category = std::random_access_iterator_tag;
-
-    pointer pos = nullptr;
-
-    explicit my_iterator(pointer ptr) : pos(ptr) {};// теперь итератор смотрит на ptr.  explicit in order to prevent implicit cast from pointer to my_iterator
-
-
-
-    bool operator==(const my_iterator& other) const { //PASS
-        return pos == other.pos; // if slots are equal return 1
-    }
-
-    bool operator!=(const my_iterator& other) const { //PASS
-        return pos != other.pos; // if slots are equal return 0
-    }
-
-    pointer operator->() { //PASS
-        return pos; // get address of a pointer
-    }
-
-    reference operator*() const { //PASS
-        return *pos; //return int position
-    }
-
-    my_iterator operator+(const difference_type& diff) const { //diff + iterator /10 //PASS
-        return my_iterator(pos + diff); // return iterator with position pos + diff
-    }
-
-
-
-    my_iterator operator-(const difference_type& diff) const { //iterator - diff /12 //PASS
-        return my_iterator(pos - diff); // return iterator with position pos - diff
-    }
-
-    difference_type operator-(const my_iterator& other) const { // iterator - iterator 13 //PASS
-        return std::distance(other.pos, pos);
-    } //--------
-
-    bool operator>(const my_iterator& other) const {  //PASS
-        return pos > other.pos; // look at the furthest from the origin and return 1 if the furthest 0 if other is the furthest
-    }
-
-    bool operator<(const my_iterator& other) const { //PASS
-        return pos < other.pos; // look at the furthest from the origin and return 0 if the furthest 1 if other is the furthest
-    }
-    //-----------
-    bool operator>=(const my_iterator& other) const { //PASS
-        return pos >= other.pos; // look at the furthest from the origin and return 1 if the furthest 0 if other is the furthest
-    } //also returns 1 if both point at the same element
-
-    bool operator<=(const my_iterator& other) const { //PASS
-        return pos <= other.pos; // look at the furthest from the origin and return 0 if the furthest 1 if the other is the furthest
-    }// also returns 1 if both point at the same element
-
-    reference operator[](size_t index) const { //PASS
-        return pos[index]; // returns a reference to an element pointed by index . is pos an array? yes
-    }
-
-
-
-    my_iterator& operator++() { //right side //PASS
-        ++pos;
-        return *this;
-    }
-
-    my_iterator operator++(int) { //left side //PASS
-        my_iterator tmp = *this;
-        ++pos;
-        return *this;
-    }
-
-    my_iterator& operator--() {//right side //PASS
-        --pos;
-        return *this;
-    }
-
-    my_iterator operator--(int) noexcept { //left side //PASS
-        my_iterator tmp = *this;
-        --pos;
-        return tmp;
-    }
-
-
-    my_iterator& operator+=(const difference_type& diff) { // modify pos (+=) //PASS
-        pos += diff;
-        return *this;
-    }
-
-    my_iterator& operator-=(const difference_type& diff) { // modify pos (-=) //PASS
-        pos -= diff;
-        return *this;
-    }
-
-    friend my_iterator operator+(difference_type const& diff, my_iterator const& it) {
-        return it + diff;
-    } // 11 //iterator + diff
-
-
-};
-
+#include "my_iterator.h"
+#include <stddef.h>
+#include <iostream>
 template<typename T>
 class my_vector {
+
+
+private:
+
+
+    size_t capacity_ext = 0;
+    size_t capacity_ = 0;
+    size_t number_of_elements = 0;
+    T* data_ = nullptr;
+
+    double factor = 1.5;
+
+
+
 public:
 
-    my_vector() :capacity_(1), number_of_elements(0), data_(new T[1]) { //def constructor via  initialization list instead of direct
-
+    my_vector() :capacity_(0), number_of_elements(0), data_(new T[1]) {
+        capacity_ext = 1;
+        capacity_ = 0;
     }
 
-    explicit my_vector(size_t n, const T& value) { //prevent implicit cast // constructor with args
+//    my_vector()
+
+
+    explicit my_vector(size_t n, const T& value){
         capacity_ = n;
         number_of_elements = n;
-        data_ = new T[capacity_];
+
+        data_ = new T[capacity_ + 1]; //fixed in reserve
+
         for (size_t i = 0; i < number_of_elements; ++i) {
             data_[i] = value;
         }
     }
 
-    my_vector(const my_vector& other) { // copy constructor
+    my_vector(const my_vector& other){
         capacity_ = other.capacity_;
         number_of_elements = other.number_of_elements;
-        T* tmp = new T[capacity_];
+        T* tmp = new T[capacity_ + 1];
         for (size_t i = 0; i < number_of_elements; ++i) {
             tmp[i] = other.data_[i];
         }
         data_ = tmp;
+    }  // copy constructor
+
+
+
+
+    ~my_vector() {
+        if (data_) {
+            delete[] data_;
+        }
     }
+
+
+
+
+    T& at(size_t pos) {
+
+        return data_[pos];
+
+    }
+
+    const T& at(size_t pos) const {
+        return data_[pos];
+    }
+
+
+    void clear() {
+        delete[] this->data_;
+        capacity_ = 1;
+        number_of_elements = 0;
+        data_ = new T[1];
+    }
+
+    T& operator[](size_t pos) {
+        try{
+            if (pos >= number_of_elements) throw 5.5;
+            return data_[pos];
+        }
+        catch(double) {
+            std::cout << " Out of range " << std::endl;
+        }
+    }
+
+    const T& operator[](size_t pos) const {
+        try{
+            if (pos >= number_of_elements) throw 5.5;
+            return data_[pos];
+        }
+        catch(double) {
+            std::cout << " Out of range " << std::endl;
+        }
+    }
+
+    T& front() {
+        return data_[0];
+    }
+
+    const T& front() const {
+        return data_[0];
+    }
+
+    T& back() {
+        return data_[number_of_elements - 1];
+    }
+
+    const T& back() const {
+        return data_[number_of_elements - 1];
+    }
+
+    size_t size() const {
+        return number_of_elements;
+    } // return number of elements //PASS
+
+    size_t capacity() const {
+        return capacity_;
+    } // return capacity //PASS
 
     my_vector& operator=(const my_vector& other) {  // overloading "equalize" operation for 2 vectors
         if (this == &other) std::cout << "youre trying to equalize same objects" << std::endl;
@@ -197,104 +144,45 @@ public:
             data_ = tmp;
 
         }
-
         return *this;
     }
 
-    ~my_vector() { // according to RAII if weve taken memory we have to give it back;
-        if (data_) {
-            delete[] data_;
-        }
-    }
-
-
-    T& at(size_t pos) { // returns data by index //PASS
-        return data_[pos];
-    }
-
-    const T& at(size_t pos) const { // returns data by index //PASS
-        return data_[pos];
-    }
-
-
-    void clear() { //PASS //clears entire array and invalidates pointers
-        delete[] this->data_;
-        capacity_ = 1;
-        number_of_elements = 0;
-        data_ = new T[1];
-    };
-
-    T& operator[](size_t pos) {// return element by index and reference //PASS
-        return data_[pos];
-    }
-
-    const T& operator[](size_t pos) const {
-        return data_[pos]; // return element by index in a constant way
-    }
-
-
-
-    T& front() { //PASS
-        return data_[0]; // returns the first element
-    }
-
-    const T& front() const { //PASS
-        return data_[0]; // returns the first element in a constant way
-    }
-
-    T& back() { //PASS
-        return data_[number_of_elements - 1]; // returns the last element
-    }
-
-    const T& back() const { //PASS 1st const return cant be modified 2nd const liable for non modifying class ch
-        return data_[number_of_elements - 1]; // returns the last element in a constant way
-    }
-
-    size_t size() const { // return number of elements //PASS
-        return number_of_elements;
-    }
-
-    size_t capacity() const { // return capacity //PASS
-        return capacity_;
-    }
-
-    T* data() { // return data //PASS
+    T* data() {
         return data_;
-    }
+    } // return data
 
-    const T* data() const { //return data in a constant way //PASS
+    const T* data() const {
         return data_;
-    }
+    } //return data
 
-    my_iterator<T> begin() { // returns iterator pointed at the first element //PASS
+    my_iterator<T> begin() {
+        return my_iterator<T>(&data_[0]);
+    } // returns iterator pointed at the first element //PASS
+
+    my_iterator<T> begin() const {
         return my_iterator(&data_[0]);
-    }
+    } // returns iterator pointed at the first element in a constant way //PASS
 
-    my_iterator<T> begin() const { // returns iterator pointed at the first element in a constant way //PASS
-        return my_iterator(&data_[0]);
-    }
-
-    my_iterator<T> end() { //PASS
+    my_iterator<T> end() {
         return my_iterator(&data_[number_of_elements]); // returns iterator pointed at the first element
-    }
+    } //PASS
 
-    my_iterator<T> end() const { //PASS
+    my_iterator<T> end() const {
         return my_iterator(&data_[number_of_elements]); // returns iterator pointed at the first element in a constant way
-    }
 
-    bool empty() const { // if the container is empty returns 1 //PASS
+    } //PASS
+
+    bool empty() const {
         return number_of_elements == 0;
-    }
+    } // if the container is empty returns 1 //PASS
 
-    void swap(my_vector& other) { // swapping two vectors with each other //It throws an error if the vector is not of the same type.
-
+    void swap(my_vector& other) {
         auto tmp = std::move(*this);
         *this = std::move(other);
         other = std::move(tmp);
+    }// swapping two vectors with each other //It throws an error if the vector is not of the same type.
 
-    }
-
-    void pop_back() { //PASS
+    void pop_back() {
         if (number_of_elements == 1) {
             delete[] data_;
             capacity_ = 1;
@@ -304,30 +192,33 @@ public:
         else {
             --number_of_elements;
         }
-    }
+    } //PASS
 
-    void push_back(const T& value) { //PASS
-        if (number_of_elements + 1 > capacity_) reserve(capacity_ * factor);
+    void push_back(const T& value) {
+        if (capacity_ == 1) reserve(2);
+        if (number_of_elements + 1 > capacity_){
+            if (capacity_ == 0) ++capacity_;
+            reserve(capacity_ * factor);
+        }
         data_[number_of_elements] = value;
         ++number_of_elements;
-    }
+    } //PASS
 
-    void reserve(size_t new_cap) { //PASS
-        if (new_cap <= capacity_) return;
-        // capacity_ = new_cap;
-        //cap
-
-        T* tmp = new T[new_cap];
+    void reserve(size_t new_cap) {
+        if (new_cap <= capacity_){
+            return;
+        }
+        T* tmp = new T[(new_cap * factor) + 1];
+        capacity_ext = (new_cap * factor) + 1;
         for (size_t i = 0; i < number_of_elements; ++i) {
             tmp[i] = data_[i];
         }
         delete[] data_;
         data_ = tmp;
-        capacity_ = new_cap;
-    }
+        capacity_ = new_cap * factor;
+    } //PASS
 
-
-    void resize_(size_t count, const T& value) { //PASS
+    void resize_(size_t count, const T& value) {
         size_t max_right = 0;
         size_t threshold_l = 0; //minleft
         if (count == number_of_elements) return;
@@ -358,11 +249,7 @@ public:
             return void();
         }
 
-    }
-
-
-
-
+    } //PASS
 
     my_iterator<T> insert(my_iterator<T> iter, T value) {
         auto tmp = iter - begin(); // элементов между тначалом и куда указали
@@ -370,7 +257,7 @@ public:
             reserve(number_of_elements * factor);
             iter = begin() + tmp; // ?
         }
-        auto initial_value = *iter.pos;
+        auto initial_value = *iter.get_pos();
         auto it = end();
         for (; it > iter; --it) {
             *it = *(it - 1);
@@ -380,16 +267,20 @@ public:
         return it;
     }
 
-    my_iterator<T> erase(const my_iterator<T> pos) { //PASS
+
+
+    my_iterator<T> erase(const my_iterator<T> pos) {
         for (auto it = pos; it < end() - 1; ++it) {
             *it = *(it + 1);
         }
         --number_of_elements;
         return pos;
-    }
+    } //PASS
 
 
-    my_iterator<T> erase(const my_iterator<T> first, const my_iterator<T> last) { //PASS
+
+
+    my_iterator<T> erase(const my_iterator<T> first, const my_iterator<T> last) {
         auto new_size = number_of_elements - (last - first);
         auto new_data_ = new T[capacity_];
         size_t idx = 0;
@@ -406,20 +297,33 @@ public:
         data_ = new_data_;
         number_of_elements = new_size;
         return my_iterator<T>(new_data_ + tmp);
+    } //PASS
+
+
+
+   // friend int main();
+
+   size_t get_capacity(){
+       return capacity_;
+    }
+    size_t get_n(){
+        return number_of_elements;
+    }
+    size_t get_ext(){
+        return capacity_ext;
     }
 
-    friend int main();
-
-private:
 
 
 
-    size_t capacity_ = 0;
-    size_t number_of_elements = 0;
-    T* data_ = nullptr;
 
-    size_t factor = 2;
+
+
+
+
+
 };
 
 
-#endif //INC_5THLABONLY_MY_VECTOR_Hx
+
+#endif //LR5_ISSUERESOLUTION_MY_VECTOR_H
